77d2b4b4c6029d33e6e7174c00da2fe17c9bdcfc
alltej <allan.tejano@gmail.com>
2018-05-11 11:06:53 -0400

    init

---
+++ src/main/scala/tutorial/ATDemo1.scala
@@ -0,0 +1,23 @@
+package tutorial
+
+/**
+  * @author Allan Tejano
+  *         5/11/2018
+  */
+object ATDemo1 extends App {
+  val name: String = "Hellow World"
+
+  println(name)
+
+  val address = "123 Main St"
+
+  println(address)
+
+  val age = 24
+
+  println(age)
+
+  val immutableName = name + " - TGIF"
+
+  println(immutableName)
+}
---
+++ src/main/scala/tutorial/Employee.scala
@@ -0,0 +1,9 @@
+package tutorial
+
+/**
+  * @author Allan Tejano
+  *         5/11/2018
+  */
+class Employee(name:String, age:Int) {
+
+}
