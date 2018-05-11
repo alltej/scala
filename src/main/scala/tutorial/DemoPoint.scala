6df509d01877db6d366f71a5cc135b87dd0e4bda
alltej <allan.tejano@gmail.com>
2018-05-11 12:22:46 -0400

    added a DemoPoint app

---
+++ src/main/scala/tutorial/DemoPoint.scala
@@ -0,0 +1,16 @@
+package tutorial
+
+/**
+  * @author Allan Tejano
+  *         5/11/2018
+  */
+object DemoPoint {
+
+  def main(args: Array[String]): Unit = {
+    val pt = new Point(4, 8)
+
+    pt.move(3, 5)
+  }
+
+
+}
---
+++ src/main/scala/tutorial/Point.scala
@@ -0,0 +1,17 @@
+package tutorial
+
+/**
+  * @author Allan Tejano
+  *         5/11/2018
+  */
+class Point(val x:Int, val y:Int) {
+  var xc = x
+  var yc = y
+
+  def move(dx:Int, dy:Int): Unit ={
+    xc = x + dx
+    yc = y + dy
+    println("New Point x:" + xc)
+    println("New Point y:" + yc)
+  }
+}
