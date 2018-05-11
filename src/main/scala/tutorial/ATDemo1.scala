8249f2bdf1db563829f073b0de9cf83f19f40075
alltej <allan.tejano@gmail.com>
2018-05-11 12:11:09 -0400

    init

--- src/main/scala/tutorial/ATDemo1.scala
+++ src/main/scala/tutorial/ATDemo1.scala
@@ -20,4 +20,22 @@ object ATDemo1 extends App {
   val immutableName = name + " - TGIF"
 
   println(immutableName)
+
+  val y = squareIt(25)
+  println(y)
+
+  println(calcInt(y, squareIt))
+
+  //define functions
+  def squareIt(x:Int):Int = {x*x}
+  def cubeIt(x:Int):Int = {x*x*x}
+
+  //define function that takes a function
+  def calcInt(x: Int, f:Int => Int) ={
+    f(x)
+  }
+
+  //inline function definition
+  val i: Int = calcInt(2, x => x*x)
+  println(i)
 }
